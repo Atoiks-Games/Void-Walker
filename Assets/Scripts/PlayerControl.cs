4f633b5c2b39ef6a7cf48a89b7f78a7e4604176c
PerenDarksbane <ajbimm@gmail.com>
2021-10-11 10:26:36 -0400

    Color Changes
    +Changed Background and Character Colors

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,26 +10,30 @@ public class PlayerControl : MonoBehaviour {
 	private Vector3 moveStore;
 	//Rendering
 	public Sprite defSprite;
-	public Sprite focused;
-	private SpriteRenderer spritRender;
+	public Sprite focSprite;
+	public Color defCol;
+	public Color focCol;
+	private SpriteRenderer spriteRender;
 	//Shooting
 	//Defense
 	//Pause/Menu
 
 	// Use this for initialization
 	void Start () {
-		spritRender = this.GetComponent<SpriteRenderer>();
+		spriteRender = this.GetComponent<SpriteRenderer>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		//Movment and Focus Rendering
 		if(Input.GetAxisRaw("Focus") > 0){
-			spritRender.sprite = focused;
+			spriteRender.sprite = focSprite;
+			spriteRender.color = focCol;
 			transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * fspd, Input.GetAxisRaw("Vertical") * Time.deltaTime * fspd, 0);
 			moveStore = new Vector3(0,0,0);
 		}else{
-			spritRender.sprite = defSprite;
+			spriteRender.sprite = defSprite;
+			spriteRender.color = defCol;
 			moveStore += new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * spd, Input.GetAxisRaw("Vertical") * Time.deltaTime * spd, 0);
 			Vector3 moveDelta = moveStore * Time.deltaTime * moveBuff;
 			transform.position += moveDelta;
