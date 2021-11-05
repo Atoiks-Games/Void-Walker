933e0c87cde33beffb78edd4ba305af9f311683f
PerenDarksbane <ajbimm@gmail.com>
2021-11-05 17:59:03 -0400

    Pause Menu
    + Added Pause Menu Functionality

--- Assets/Scripts/LevelStateHandler.cs
+++ Assets/Scripts/LevelStateHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelStateHandler : MonoBehaviour
 {
@@ -14,8 +15,9 @@ public class LevelStateHandler : MonoBehaviour
 
     public GameObject LevelLoaderObject;
     private LevelLoader _levelLoader;
-
+    public static bool IsPaused = false;
     private GameState _currentGameState = GameState.None;
+    public GameObject PauseMenu;
 
 
     // Start is called before the first frame update
@@ -30,10 +32,15 @@ public class LevelStateHandler : MonoBehaviour
         GameObject playerObject = _levelLoader.LoadLevel("/Resources/EnemySpawnTest.json", this.transform);
         playerObject.GetComponent<PlayerControl>().AddDeathCallback(OnPlayerDeath);
         _currentGameState = GameState.Alive;
+        PauseMenu.transform.Find("Quit").gameObject.GetComponent<Button>().onClick.AddListener(Quit);
+        PauseMenu.transform.Find("Continue").gameObject.GetComponent<Button>().onClick.AddListener(Resume);
+        PauseMenu.transform.Find("Restart").gameObject.GetComponent<Button>().onClick.AddListener(Resume);
+        PauseMenu.transform.Find("Restart").gameObject.GetComponent<Button>().onClick.AddListener(RestartLevel);
     }
 
     private void RestartLevel()
     {
+        Time.timeScale = 1f;
         foreach (Transform childTransform in transform)
         {
             Destroy(childTransform.gameObject);
@@ -52,10 +59,34 @@ public class LevelStateHandler : MonoBehaviour
                 RestartLevel();
             }
         }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused){
+                Resume();
+            } else {
+                Pause();
+            }
+            IsPaused = !IsPaused;
+        }
     }
 
     private void OnPlayerDeath(GameObject player)
     {
         _currentGameState = GameState.GameOver;
+    }
+
+    private void Resume(){
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    private void Pause(){
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void Quit(){
+        Debug.Log("If we weren't in the editor, I would have quit.");
+        Application.Quit();
     }
 }
